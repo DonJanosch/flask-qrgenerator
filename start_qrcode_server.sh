948fcda4d9cd625dcef64c419c49dde1d122418e
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 12:10:56 +0200

    Changed log file to be non interactive

--- start_qrcode_server.sh
+++ start_qrcode_server.sh
@@ -2,5 +2,5 @@ git clone https://github.com/DonJanosch/flask-qrgenerator.git app
 cd app
 docker build -t flask-qrgenerator:latest .
 docker run -d -p 80:8080 --restart always --name qrgenerator flask-qrgenerator
-docker logs --follow qrgenerator
+docker logs qrgenerator
 echo "All Done, Container is up and running"
