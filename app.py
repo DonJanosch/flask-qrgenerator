bf72acbbf36a279cab71c25cef8dd449496094b5
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 15:11:38 +0200

    Changed Colors back to black/white from planemos colors

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def index():
         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
         qr.add_data(input_data)
         qr.make(fit=True)
-        img = qr.make_image(fill_color='#66e100',back_color="white")
+        img = qr.make_image(fill_color="black",back_color="white")#(fill_color='#66e100',back_color="white") #use planemos colors => impacts scanner results negativgely, deactivated.
         return serve_pil_image(img)
 
 if __name__ == '__main__':
