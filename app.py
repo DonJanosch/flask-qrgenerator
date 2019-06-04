19e0b3a59aae4678745a60ba32efcd7e9b30082f
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 22:58:07 +0200

    Made the code of app.py more pythonic.

--- app.py
+++ app.py
@@ -2,21 +2,29 @@ from flask import Flask, request, send_file
 import qrcode
 from io import BytesIO
 
-form_name = 'data'
-input_form = f'<h1>String to QR-Code</h1><p>By Jan Macenka</p><br><h2>Input some text and hit submit:</h2><form action="/" method="POST"><input name="{form_name}"><input type="submit"></form>'
+DATA_FIELD = 'data'
+INPUT_FORM = f'''
+<h1>String to QR-Code</h1>
+<p>By Jan Macenka</p>
+<h2>Input some text and hit submit:</h2>
+<form action="/" method="POST">
+<input name="{DATA_FIELD}">
+<input type="submit">
+</form>'''
 
-app =Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return input_form
+        return INPUT_FORM
     elif request.method == 'POST':
         imgIO = BytesIO()
-        input_data = request.form[form_name]
-        qrcode.make(input_data).save(imgIO, 'PNG', quality=70);
+        input_data = request.form[DATA_FIELD]
+        img = qrcode.make(input_data)
+        img.save(imgIO, 'PNG', quality=70)
         imgIO.seek(0)
         return send_file(imgIO, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
