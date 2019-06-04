5ad50373634ada981d714958979cc1731b4cb49b
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 14:01:38 +0200

    Minor changes to inprove reporting during setup-process

--- start_qrcode_server.sh
+++ start_qrcode_server.sh
@@ -1,6 +1,8 @@
+echo "Pulling the project from github"
 git clone https://github.com/DonJanosch/flask-qrgenerator.git app
 cd app
+echo "Building and launching the container"
 docker build -t flask-qrgenerator:latest .
 docker run -d -p 80:8080 --restart always --name qrgenerator flask-qrgenerator
-docker logs --tail * qrgenerator
+echo $(docker logs qrgenerator)
 echo "All Done, Container is up and running"
